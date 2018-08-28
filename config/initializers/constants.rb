ac847e69f3ca6ac569e4d3440eb2b28ddfa3fc97
Sulman Baig <sulmanweb@gmail.com>
2018-08-28 22:56:32 +0500

    Setting cors and creating constants initializer

---
+++ config/initializers/constants.rb
@@ -0,0 +1,1 @@
+# App Constants go here
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# Restricting cors for sulmanbaig.com and localhost origins
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost*', '127.0.0.1*', '*.sulmanbaig.com'
+    resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete, :patch], expose: %w[Authorization expiry]
+  end
+end
